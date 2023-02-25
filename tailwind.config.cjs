0774ea67272b35d984aa3e0bd4e3d9e5dd90d4af
German Fernandez <germanfernandez0306@gmail.com>
2023-02-25 15:53:35 -0300

    feat: add colors to tailwind

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,13 @@
 module.exports = {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx,svelte}'],
   theme: {
-    extend: {}
+    extend: {
+      colors: {
+        primary: '#3F4145',
+        secondary: '#5E5E5E',
+        tertiary: '#878787'
+      }
+    }
   },
   plugins: []
 }
