9604e514b763a46a86cd8194ce65a8d0e0b2cd1d
German Fernandez <germanfernandez0306@gmail.com>
2023-02-25 15:53:47 -0300

    tweak: remove global styles from svelte

--- src/App.svelte
+++ src/App.svelte
@@ -1,38 +1,10 @@
 <script>
-  import Header from './components/Header.svelte'
-  import NotesGrid from './components/NotesGrid.svelte'
+  import AsideMenu from './components/AsideMenu.svelte'
+  import NotesSection from './components/NotesSection.svelte'
 </script>
 
-<Header />
-<NotesGrid />
-
-<style>
-  :global(:root) {
-    --color-blue: #2268eb;
-    --color-gray: #6b6868;
-
-    font-family: 'Signika Negative', system-ui, -apple-system,
-      BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell,
-      'Open Sans', 'Helvetica Neue', sans-serif;
-    font-size: 16px;
-    line-height: 24px;
-    font-weight: 400;
-    font-synthesis: none;
-    text-rendering: optimizeLegibility;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-    -webkit-text-size-adjust: 100%;
-  }
-
-  :global(body) {
-    margin: 0;
-    padding: 0;
-    filter: invert(100%);
-    background-color: black;
-  }
-
-  :global(::selection) {
-    background: var(--color-blue);
-    color: #fff;
-  }
-</style>
+<main
+  class="min-h-screen grid md:grid-cols-[minmax(300px,_1fr)_3fr] p-6  gap-8 text-tertiary">
+  <AsideMenu />
+  <NotesSection />
+</main>
